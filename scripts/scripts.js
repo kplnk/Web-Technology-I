bb37aa06a199254f235c6256adea3a954258f5fc
Kplunk <klunk@hawaii.edu>
2026-05-03 21:08:20 -1000

    implement hamburger menu

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,11 @@
-function hamburger() {
-    var menu = document.getElementById("menu-links");
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
+function menu() {
+    var navlinks = document.getElementById("nav-links");
+    var menuicon = document.getElementById("icon");
+    if (navlinks.style.display === "block") {
+        navlinks.style.display = "none";
+        menuicon.style.color = "#262626";
     } else {
-        menu.style.display = "block";
+        navlinks.style.display = "block";
+        menuicon.style.color = "#4d4d4d";
     }
 }
