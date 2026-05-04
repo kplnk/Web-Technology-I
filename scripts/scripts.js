1d772e1d3736ca3eff6af021f38d761745ebd219
Kplunk <klunk@hawaii.edu>
2026-05-03 15:25:08 -1000

    revert changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,11 +1,9 @@
-//Hamburger menu function
-function menu() {
-    var navlinks = document.getElementById("nav-links");
-    var menuicon = document.getElementById("icon");
-    if (navlinks.style.display === "block") {
-        navlinks.style.display = "none";
+//Hamburger menu script
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
     } else {
-        navlinks.style.display = "block";
-        menuicon.style.color = "#f6eee4";
+        menu.style.display = "block";
     }
 }
