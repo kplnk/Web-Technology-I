4610aceca421c0782ca338a514083c10388b710c
Kplunk <klunk@hawaii.edu>
2026-05-03 14:40:02 -1000

    remove tab-desk from nav in index

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,7 +4,6 @@ function menu() {
     var menuicon = document.getElementById("icon");
     if (navlinks.style.display === "block") {
         navlinks.style.display = "none";
-        menuicon.style.color = "#2a1f14";
     } else {
         navlinks.style.display = "block";
         menuicon.style.color = "#f6eee4";
